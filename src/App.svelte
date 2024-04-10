5d6edd3b491002c734859c62fde758c36d8a3349
larabarijhoff <lara.barijhoff1@gmail.com>
2024-04-10 11:19:04 -0300

    1era version

--- src/App.svelte
+++ src/App.svelte
@@ -3,146 +3,41 @@
   import {onMount} from "svelte"
 
   /* Array donde guardaremos la data */
-  let deportistas = []
+  let caras =[]
 
-  /* 1. Escala para edades */
-  let grosor = d3.scaleLinear().range([5, 20])
+  let color_pelo = d3
+    .scaleLineal().range([0, 0.9])
 
-  /* 2. Escala para genero */
-  let colorGenero = d3
+  let color_figura = d3
     .scaleOrdinal()
-    .domain(["F", "M"])
-    .range(["#ffc0cb", "#c0f9ff"])
+    .domain(["Netflix","Prime","Max", "Star +","National Geographic"])
+    .range(["#e50914", "#00a8e1", "#50128f", "#ff6e00","ffce00"])
 
-  /* 3. Escala para continentes */
-  let colorContinentes = d3
+  let iluminacion = d3
+    .scaleLineal()
+    .domain([1,0])
+    .range([0.35,0.80])
+
+  let figura = d3
     .scaleOrdinal()
-    .domain(["América", "África", "Asia", "Europa", "Oceanía"])
-    .range(["#ed334e", "#000000", "#fbb132", "#009fe3", "#00963f"])
+    .domain(["Comedia", "Terror", "Documental","Suspenso","Romance","Rom-Com","Sci-Fi"])
+    .range
 
-  /* 4. Escala para altura */
-  let radioAltura = d3.scaleRadial()
+ 
 
-  /* 5. Escala para medallas */
-  let colorMedalla = d3.scaleOrdinal()
-    .domain(["Oro", "Plata", "Bronce"])
-    .range(["gold", "silver", "brown"])
 
-  onMount(() => {
-    d3.csv("./data/deportistas.csv", d3.autoType).then(data => {
-      console.log(data)
 
-      /* Actualizamos dominio con la data de edad */
-      let minMaxEdad = d3.extent(data, d => d.edad)
-      grosor = grosor.domain(minMaxEdad)
+  
 
-      /* Actualizamos dominio y rango con la data de altura */
-      let minMaxAltura = d3.extent(data, d => d.altura)
-      radioAltura = radioAltura.domain(minMaxAltura).range([25, 50])
-
-      deportistas = data
-    })
-  })
+  
 </script>
 
 <main>
-  <div class="header">
-    <img src="/images/olympics-logo.png" width="100" alt="anillos" />
-    <h3 class="headline">
-      <b>Triunfos Olímpicos</b>
-      Medallas, alturas y continentes
-    </h3>
-    <p class="bajada">Explorando los logros olímpicos a través de datos</p>
-  </div>
+  opacity para la luminosidad de figuras 
 
-  <!-- Conedor de las entidades -->
-  <div class="container">
-    <!-- Iteramos la data para visualizar c/ entidad -->
-    {#each deportistas as dep}
-      <div class="person-container">
-        <div class="medal"
-          style="background-color: {colorMedalla(dep.medalla)}"
-          ></div>
-        <div
-          class="person"
-          style="border-width: {grosor(dep.edad)}px; 
-        background-color:{colorGenero(dep.genero)}; 
-        width: {2 * radioAltura(dep.altura)}px; 
-        height: {2 * radioAltura(dep.altura)}px; 
-        border-color: {colorContinentes(dep.continente)}"
-        ></div>
-        <p class="name">
-          <b>{dep.nombre}</b>
-          <br />
-          {dep.continente}
-        </p>
-      </div>
-    {/each}
-  </div>
 </main>
 
 <style>
-  .header {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin-top: 50px;
-    margin-bottom: 80px;
-  }
-  .headline {
-    font-size: 30px;
-    line-height: 1.2;
-    font-weight: normal;
-    text-align: center;
-    margin: 20px;
-  }
-  .bajada {
-    font-size: 18px;
-    font-weight: normal;
-    text-align: center;
-    margin: 10px;
-  }
-  .headline b {
-    display: block;
-  }
-  .container {
-    display: flex;
-    justify-content: center;
-    align-items: end;
-    margin: auto;
-    flex-wrap: wrap;
-    max-width: 1000px;
-    gap: 30px;
-    margin-bottom: 100px;
-  }
-  .person-container {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    flex: 180px 0 0;
-  }
-  .person {
-    width: 100px;
-    height: 100px;
-    border: 10px solid black;
-    border-radius: 50%;
-    box-sizing: border-box;
-    background-color: pink;
-  }
-  .medal {
-    width: 15px;
-    height: 15px;
-    border-radius: 50%;
-    background-color: gold;
-    margin: 5px 0;
-  }
-  .name {
-    font-size: 14px;
-    color: rgb(65, 65, 65);
-    font-weight: normal;
-    text-align: center;
-    margin-top: 5px;
-  }
+
+
 </style>
