38de86d428a414a96c62494144e5b9abde603671
Camilo Suárez <camilodsuarez@gmail.com>
2024-05-14 19:47:16 -0300

    Revert "multiPages vite"

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import multiPages from 'vite-plugin-multi-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte(),
-    multiPages({
-      // Configuration options for vite-plugin-multi-pages
-      pagesDir: './', // Directory where your page components are located
-      importMode: 'async', // Import mode for dynamic imports ('sync' or 'async')
-    })
-  ]
+  plugins: [svelte()],
 })
