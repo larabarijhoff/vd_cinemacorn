1c4ef2db5de186640ac2ab0267108861bbdb6cbd
Camilo Suárez <camilodsuarez@gmail.com>
2024-05-14 18:27:13 -0300

    multiPages vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import multiPages from 'vite-plugin-multi-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    multiPages({
+      // Configuration options for vite-plugin-multi-pages
+      pagesDir: './', // Directory where your page components are located
+      importMode: 'async', // Import mode for dynamic imports ('sync' or 'async')
+    })
+  ]
 })
